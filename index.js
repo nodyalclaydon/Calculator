8370e96106c9f8ae0143bad36e91a8a94dfc3750
nodyalclaydon <72510779+nodyalclaydon@users.noreply.github.com>
2021-01-13 15:45:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 let displayNum = ""
+let invisibleNum = ""
 const display = document.querySelector(".display")
-const btn = document.querySelectorAll(".btn")
+const numBtn = document.querySelectorAll(".num")
+const redBtn = document.querySelectorAll(".red")
 
-for(i =0; i < btn.length; i++){
-    btn[i].addEventListener("click", handleClick)
+for(i =0; i < numBtn.length; i++){
+    numBtn[i].addEventListener("click", handleNumClick)
 }
 
-function handleClick(e) {
+for(i =0; i < redBtn.length; i++){
+    redBtn[i].addEventListener("click", handleRedClick)
+}
+
+function handleNumClick(e) {
     const key = e.target.innerHTML
-    if ((displayNum === "0") || (displayNum === "Err")) {
+    if ((displayNum === "0") || (displayNum != invisibleNum)) {
         displayNum = key
+        invisibleNum = invisibleNum + key
         display.innerHTML = displayNum
     } if (displayNum.length >= 9) {
-        displayNum = "Err"
-        display.innerHTML = displayNum
+        displayNum = "0"
+        invisibleNum = ""
+        display.innerHTML = "Err"
     } else {
         displayNum = displayNum + key
+        invisibleNum = invisibleNum + key
         display.innerHTML = displayNum
     }
+}
+
+function handleRedClick(e) {
+    const key = e.target.innerHTML
+    invisibleNum = invisibleNum + key
+    console.log(invisibleNum)
 }
 
 document.querySelector(".clear").addEventListener("click", () => {
     displayNum = "0"
     display.innerHTML = displayNum
+    console.log(displayNum)
 })
 
 document.getElementById("calculate").addEventListener("click", equate)
 
 function equate() {
-    displayNum = eval(displayNum)
+    invisibleNum = eval(invisibleNum)
+    displayNum = invisibleNum
     display.innerHTML = displayNum
 }
